43b51d4c3d4ef16ba91925ecc50a04f302946185
Patrik Oskarsson <patrik.oskarsson@darpal.com>
2020-08-01 22:36:17 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// Get quote from API
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        if (data.quoteAuthor == '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        if (data.quoteText.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText + '=' + data.quoteText.length;
+
+    } catch (error) {
+        //getQuote();
+    }
+}
+
+// Onload
+getQuote();
